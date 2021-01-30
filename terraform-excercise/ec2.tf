55665f98d2af5fbed4dce3d5eed2c521f60c1f00
karankaran1908 <karanagarwal@MacBook-Pro.local>
2021-01-30 15:03:20 -0500

    Added new changes

--- terraform-excercise/ec2.tf
+++ terraform-excercise/ec2.tf
@@ -3,22 +3,27 @@ ami = "ami-0fc61db8544a617ed"
 instance_type = "t2.micro"
 subnet_id = aws_subnet.main.id
 
-provisioner "remote-exec" {
-connection {
-host = aws_instance.CentOS_instance.public_ip
+associate_public_ip_address = true
+
 }
-        inline = [
-        "sudo yum install httpd node docker git -y",
-        "sudo git clone https://github.com/CitrineInformatics/sample-service.git",
-        "sudo systemctl start httpd",
-        "sudo systemctl enable httpd",
-        "sudo systemctl start docker",
-        "sudo systemctl enable docker",
-        "cd /home/ubuntu/sample-service",
-        "sudo docker build . -t newimage",
-        "sudo docker run -p 80:5000 -d newimage",
-        ]
-    }
+
+resource "aws_security_group" "example" {
+  name = var.instance_name
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 output "IP"{
@@ -43,3 +48,32 @@ resource "aws_subnet" "main" {
   }
 }
     
+resource "null_resource" "example_provisioner" {
+  triggers = {
+    public_ip = aws_instance.CentOS_instance.public_ip
+  }
+
+  connection {
+    type  = "ssh"
+    host  = aws_instance.CentOS_instance.public_ip
+    user  = ubuntu
+    port  = 22
+    agent = true
+  }
+
+  // change permissions to executable and pipe its output into a new file
+  provisioner "remote-exec" {
+    inline = [
+        "sudo yum install httpd node docker git -y",
+        "sudo git clone https://github.com/CitrineInformatics/sample-service.git",
+        "sudo systemctl start httpd",
+        "sudo systemctl enable httpd",
+        "sudo systemctl start docker",
+        "sudo systemctl enable docker",
+        "cd /home/ubuntu/sample-service",
+        "sudo docker build . -t newimage",
+        "sudo docker run -p 80:5000 -d newimage",
+        ]
+  }
+
+}
